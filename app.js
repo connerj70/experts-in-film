c437e96f278cacd3844577cbb7e9cfc8dce38ebb
connerj70 <connerj70@gmail.com>
2018-01-14 18:15:49 -0700

    pefection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var hamburgerButton = document.querySelector(".hamburger");
 var openSB = document.querySelector("#openSB");
 var slider = document.querySelector(".nav-wrapper");
+var sliderImages = document.querySelectorAll(".carver_tickets");
+var sliderCaptions = document.querySelectorAll(".carver_tickets_caption");
 
 hamburgerButton.addEventListener('click', function() {
     slider.classList.remove('nav-wrapper-active');
@@ -10,3 +12,54 @@ openSB.addEventListener('click', function() {
     slider.classList.add('nav-wrapper-active');
 });
 
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+function checkSlide(e) {
+    sliderImages.forEach(function(sliderImage) {
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+
+        if(isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+
+
+    });
+}   
+
+function checkSlide2(e) {
+    sliderCaptions.forEach(function(sliderImage) {
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+
+        if(isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+
+
+    });
+}   
+
+window.addEventListener('scroll', debounce(checkSlide, 20, true));
+window.addEventListener('scroll', debounce(checkSlide2, 20, true));
