c4a5fb71145366a0c81535d7dfd21bf0ca93404f
jharris1829 <jharris1829@gmail.com>
2018-01-17 21:14:45 -0700

    Joshs crap

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var hamburgerButton = document.querySelector(".hamburger");
 var openSB = document.querySelector("#openSB");
 var slider = document.querySelector(".nav-wrapper");
+var main = document.querySelector("body");
 
 hamburgerButton.addEventListener('click', function() {
     slider.classList.remove('nav-wrapper-active');
+    main.style.overflowY = "scroll";
 });
 
 openSB.addEventListener('click', function() {
     slider.classList.add('nav-wrapper-active');
+    main.style.overflowY = "hidden";
 });
 
